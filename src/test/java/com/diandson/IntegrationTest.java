db1dbd26446abe1f557b2d502fb43e0f2b801fea
Diandson <mdianda.md@gmail.com>
2022-02-08 16:55:56 +0000

    Initial version of appPharma generated by generator-jhipster@7.6.0

---
+++ src/test/java/com/diandson/IntegrationTest.java
@@ -0,0 +1,17 @@
+package com.diandson;
+
+import com.diandson.AppPharmaApp;
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * Base composite annotation for integration tests.
+ */
+@Target(ElementType.TYPE)
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootTest(classes = AppPharmaApp.class)
+public @interface IntegrationTest {
+}
