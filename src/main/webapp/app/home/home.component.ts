0e4b15597dd7ccac36b16740167994d6a7d68b15
Diandson <mdianda.md@gmail.com>
2022-02-17 16:33:33 +0000

    implementation de vente pour serveur

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,13 +1,15 @@
-import { Component, OnDestroy, OnInit,} from '@angular/core';
-import {Router} from '@angular/router';
-import {Subject} from 'rxjs';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import { Router } from '@angular/router';
+import { map, Observable, of, startWith, Subject } from 'rxjs';
 
-import {AccountService} from 'app/core/auth/account.service';
-import {Account} from 'app/core/auth/account.model';
-import {MedicamentService} from "../entities/medicament/service/medicament.service";
-import {IMedicament} from "../entities/medicament/medicament.model";
-import {Authority} from "../config/authority.constants";
-import {IVenteMedicament, VenteMedicament} from "../entities/vente-medicament/vente-medicament.model";
+import { AccountService } from 'app/core/auth/account.service';
+import { Account } from 'app/core/auth/account.model';
+import { MedicamentService } from '../entities/medicament/service/medicament.service';
+import { IMedicament } from '../entities/medicament/medicament.model';
+import { Authority } from '../config/authority.constants';
+import { IVenteMedicament } from '../entities/vente-medicament/vente-medicament.model';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { NzModalService } from 'ng-zorro-antd/modal';
 
 @Component({
   selector: 'jhi-home',
@@ -19,25 +21,30 @@ export class HomeComponent implements OnInit, OnDestroy {
   medicaments?: IMedicament[];
   medicamentSelect?: IMedicament;
   medicamentVendu?: IVenteMedicament[];
-  medicamentList?: IMedicament[];
+  medicamentList: IMedicament[] = [];
+  filteredMedicament?: Observable<IMedicament[]>;
+
+  @ViewChild('input') input: any;
+  prixTotal = 0;
+
   private readonly destroy$ = new Subject<void>();
 
   constructor(
     protected medicamentService: MedicamentService,
     private accountService: AccountService,
+    private modalService: NgbModal,
+    private modal: NzModalService,
     private router: Router
-  ) {
-  }
+  ) {}
 
   ngOnInit(): void {
-    this.accountService.getAuthenticationState()
-      .subscribe(account => (this.account = account));
+    this.accountService.getAuthenticationState().subscribe(account => (this.account = account));
 
-    if (this.accountService.hasAnyAuthority([Authority.SERVEUR, Authority.STRUCTURE_ADMIN])){
-      this.medicamentService.query()
-        .subscribe( res => {
-          this.medicaments = res.body ?? []
-        })
+    if (this.accountService.hasAnyAuthority([Authority.SERVEUR, Authority.STRUCTURE_ADMIN])) {
+      this.medicamentService.query().subscribe(res => {
+        this.medicaments = res.body ?? [];
+        this.filteredMedicament = of(this.medicaments);
+      });
     }
   }
 
@@ -50,25 +57,89 @@ export class HomeComponent implements OnInit, OnDestroy {
     this.destroy$.complete();
   }
 
-  isAuthenticated(): boolean{
+  isAuthenticated(): boolean {
     return this.accountService.isAuthenticated();
   }
 
-  getSelectedMedicament($event: IMedicament): void {
-    const existMedicament = this.medicamentList?.find(m => m.dci === $event.dci);
-    if (existMedicament){
-      existMedicament.stockTheorique = existMedicament.stockTheorique! + 1
-    }else {
-      $event.stockTheorique = 1;
-      const vm = new VenteMedicament();
-      vm.medicament = $event;
-      // vm.montant = String($event.prixPublic);
-      vm.quantite = $event.stockTheorique;
+  getFilteredOptions(value: string): Observable<IMedicament[]> {
+    return of(value).pipe(
+      startWith(''),
+      map(filterString => this.filter(filterString))
+    );
+  }
+  onChange(): any {
+    this.filteredMedicament = this.getFilteredOptions(this.input.nativeElement.value);
+  }
 
-      this.medicamentList?.push($event);
-      this.medicamentVendu?.push(vm);
+  onSelectionChange($event: IMedicament): any {
+    this.filteredMedicament = this.getFilteredOptions($event.dci!.toString());
 
-      alert(this.medicamentList?.length);
+    if ($event.stockTheorique! > 0) {
+      const medicam = this.medicamentList.find(ex => ex.id === $event.id);
+      if (medicam) {
+        medicam.stockTheorique = medicam.stockTheorique! + 1;
+        this.prixTotal = this.prixTotal + medicam.prixPublic! / (medicam.stockTheorique - 1);
+        // medicam.prixPublic = (medicam.stockTheorique * $event.prixPublic!) / (medicam.stockTheorique - 1);
+      } else {
+        $event.stockTheorique = 1;
+        this.medicamentList.push($event);
+        this.medicamentList.sort().reverse();
+        this.prixTotal = this.prixTotal + $event.prixPublic!;
+      }
+    } else {
+      this.warning('Le stock de ce médicament est epuisé veuillez vous reapprovisionner!');
     }
+    this.input.nativeElement.value = null;
+  }
+
+  success(msg: string): void {
+    this.modal.success({
+      nzContent: msg,
+      nzTitle: 'SUCCESS',
+      nzOkText: 'OK',
+    });
+  }
+  warning(msg: string): void {
+    this.modal.warning({
+      nzContent: msg,
+      nzTitle: 'ATTENTION',
+      nzOkText: 'OK',
+    });
+  }
+  error(msg: string): void {
+    this.modal.error({
+      nzContent: msg,
+      nzTitle: 'ERROR',
+      nzOkText: 'OK',
+    });
+  }
+
+  detailMedic(medi: IMedicament): void {
+    this.medicamentSelect = medi;
+  }
+
+  getQuantite(): void {
+    this.prixTotal = 0;
+    this.medicamentList.forEach(value => (this.prixTotal = this.prixTotal + value.stockTheorique! * value.prixPublic!));
+  }
+
+  remove(medi: IMedicament): void {
+    this.modal.warning({
+      nzContent: 'Voulez-vous vraiment retirer ce produit?',
+      nzTitle: 'ATTENTION',
+      nzOkText: 'Oui',
+      nzCancelText: 'Non',
+      nzOnOk: () => {
+        this.prixTotal = 0;
+        const index = this.medicamentList.findIndex(value => value.id === medi.id);
+        this.medicamentList.splice(index, 1);
+        this.medicamentList.forEach(value => (this.prixTotal = this.prixTotal + value.prixPublic!));
+      },
+    });
+  }
+
+  private filter(value: string): IMedicament[] {
+    const filterValue = value.toLowerCase();
+    return this.medicaments!.filter(medi => medi.dci!.toLowerCase().lastIndexOf(filterValue) === 0);
   }
 }
--- src/main/webapp/app/home/home.module.ts
+++ src/main/webapp/app/home/home.module.ts
@@ -4,16 +4,54 @@ import { RouterModule } from '@angular/router';
 import { SharedModule } from 'app/shared/shared.module';
 import { HOME_ROUTE } from './home.route';
 import { HomeComponent } from './home.component';
-import {NzInputModule} from "ng-zorro-antd/input";
-import {NzButtonModule} from "ng-zorro-antd/button";
-import {NzIconModule} from "ng-zorro-antd/icon";
-import {NzAutocompleteModule} from "ng-zorro-antd/auto-complete";
-import {NbAutocompleteModule, NbCardModule, NbInputModule, NbOptionModule} from "@nebular/theme";
-import {NzGridModule} from "ng-zorro-antd/grid";
-import {NzSelectModule} from "ng-zorro-antd/select";
+import { NzInputModule } from 'ng-zorro-antd/input';
+import { NzButtonModule } from 'ng-zorro-antd/button';
+import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzAutocompleteModule } from 'ng-zorro-antd/auto-complete';
+import {
+  NbAutocompleteModule,
+  NbButtonModule,
+  NbCardModule,
+  NbFormFieldModule,
+  NbIconModule,
+  NbInputModule,
+  NbOptionModule,
+} from '@nebular/theme';
+import { NzGridModule } from 'ng-zorro-antd/grid';
+import { NzSelectModule } from 'ng-zorro-antd/select';
+import { MatCardModule } from '@angular/material/card';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatButtonModule } from '@angular/material/button';
+import { NzListModule } from 'ng-zorro-antd/list';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatIconModule } from '@angular/material/icon';
+import { InputNumberModule } from 'primeng/inputnumber';
 
 @NgModule({
-  imports: [SharedModule, RouterModule.forChild([HOME_ROUTE]), NzInputModule, NzButtonModule, NzIconModule, NzAutocompleteModule, NbInputModule, NbCardModule, NzGridModule, NzSelectModule, NbOptionModule, NbAutocompleteModule],
+  imports: [
+    SharedModule,
+    RouterModule.forChild([HOME_ROUTE]),
+    NzInputModule,
+    NzButtonModule,
+    NzIconModule,
+    NzAutocompleteModule,
+    NbInputModule,
+    NbCardModule,
+    NzGridModule,
+    NzSelectModule,
+    NbOptionModule,
+    NbAutocompleteModule,
+    MatCardModule,
+    MatChipsModule,
+    NbFormFieldModule,
+    NbButtonModule,
+    NbIconModule,
+    MatButtonModule,
+    NzListModule,
+    MatFormFieldModule,
+    MatIconModule,
+    InputNumberModule,
+  ],
   declarations: [HomeComponent],
 })
 export class HomeModule {}
